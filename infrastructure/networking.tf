d83f2861524f20647b8e9f40440c96b5067486dd
Fawzi Hmouda <h.fawzi@hotmail.com>
2022-05-12 02:33:48 +1000

    add VPC

---
+++ infrastructure/networking.tf
@@ -0,0 +1,8 @@
+###---VPC---###
+resource "google_compute_network" "vpc" {
+  name = "vpc-gke"
+  description = "VPC Network For GKE Cluster"
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
+
+}
