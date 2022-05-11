2ac75685e8f05e787d874402cce0b7b40c27a185
Fawzi Hmouda <h.fawzi@hotmail.com>
2022-05-12 02:06:50 +1000

    add workflow

---
+++ infrastructure/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "gcs" {
+    bucket  = "terraformstate-fawzihmouda"
+    prefix  = "state"
+  }
+
+
+}
+
+provider "google" {
+  project     = "fawzihmouda-dev"
+  region      = "australia-southeast1"
+}
+
