fde4ce3eb42849f329cceee83815ef724ec567ce
Fawzi Hmouda <h.fawzi@hotmail.com>
2022-05-12 02:47:33 +1000

    add subnetwork

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -1,7 +1,13 @@
 ###---VPC---###
 resource "google_compute_network" "vpc" {
-  name = "vpc-gke"
-  description = "VPC Network For GKE Cluster"
+  name                    = "vpc-gke"
+  description             = "VPC Network For GKE Cluster"
   auto_create_subnetworks = false
-  routing_mode = "REGIONAL"
+  routing_mode            = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "gke_subnet_A" {
+  name          = "gke_subnet"
+  ip_cidr_range = "192.168.0.0/16"
+  network       = google_compute_network.vpc.id
 }
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraformstate-fawzihmouda"
-    prefix  = "state"
+    bucket = "terraformstate-fawzihmouda"
+    prefix = "state"
   }
-  
+
 }
 
 provider "google" {
-  project     = "fawzihmouda-dev"
-  region      = "australia-southeast1"
+  project = "fawzihmouda-dev"
+  region  = "australia-southeast1"
 }
 
