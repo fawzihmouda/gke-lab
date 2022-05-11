304c0f2bc49503872411bc42fae6fd6ba1699a1a
Fawzi Hmouda <h.fawzi@hotmail.com>
2022-05-12 03:08:27 +1000

    add subnetwork

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,4 +1,8 @@
 terraform {
+  required_providers {
+    google = {
+      version = "~> 3.90.0"
+    }
   backend "gcs" {
     bucket = "terraformstate-fawzihmouda"
     prefix = "state"
@@ -10,4 +14,4 @@ provider "google" {
   project = "fawzihmouda-dev"
   region  = "australia-southeast1"
 }
-
+}
